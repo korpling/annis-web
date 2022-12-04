30dc39b13fc48a4869b5e0c4f8e4ef2606778c14
Thomas Krause <thomaskrause@posteo.de>
2022-12-04 13:39:49 +0100

    Add test coverage script

---
+++ test_coverage.sh
@@ -0,0 +1,21 @@
+# Make sure you installed grcov via cargo first and that the llvm tools are available
+# 
+# cargo install grcov
+# rustup component add llvm-tools-preview
+
+# Set some environment variables needed by grcov
+export CARGO_INCREMENTAL=0
+export RUSTFLAGS='-Cinstrument-coverage'
+
+mkdir -p target/coverage/
+
+# Run all tests
+LLVM_PROFILE_FILE='cargo-test-%p-%m.profraw' cargo test
+
+# Generate HTML report in target/debug/coverage/index.html
+grcov . --binary-path ./target/debug/deps/ -s . -t html --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/html/
+# Also generate a lcov file for further processing
+grcov . --binary-path ./target/debug/deps/ -s . -t lcov --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/tests.lcov
+
+# Cleanup
+find . -name '*.profraw' -delete
