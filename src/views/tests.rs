502b26d5b1fe54259407e8dafe6338a9a8b495c6
Thomas Krause <thomaskrause@posteo.de>
2022-12-08 19:16:59 +0100

    Add tests for the error page when no corpus list can be shown

--- src/views/tests.rs
+++ src/views/tests.rs
@@ -52,13 +52,57 @@ async fn service_down() {
         assert_eq!(response.status(), StatusCode::BAD_GATEWAY);
 
         let html = get_html(response).await;
-        let list_selector = Selector::parse(".content > ul > li").unwrap();
-        let corpora: Vec<_> = html
-            .select(&list_selector)
-            .map(|e| e.text().collect::<Vec<_>>().join(""))
-            .collect();
 
-        assert_eq!(vec!["demo.dialog", "pcc2"], corpora);
+        // Two tiles should exist, one with the error description and an info
+        // box what to do about it.
+        let tile_selector = Selector::parse("article.tile.is-child").unwrap();
+        let tiles: Vec<_> = html.select(&tile_selector).collect();
+        assert_eq!(2, tiles.len());
+
+        // Check the title (with proper status code) and that the error message
+        // is displayed
+        let subtitle_selector = Selector::parse("p.subtitle").unwrap();
+        let subtitle_error = tiles[0]
+            .select(&subtitle_selector)
+            .next()
+            .unwrap()
+            .text()
+            .next()
+            .unwrap()
+            .trim();
+        assert_eq!(
+            concat!(
+                "The code of the error is 502 (Bad Gateway) and the following ",
+                "message describes the issue in more detail:"
+            ),
+            subtitle_error
+        );
+
+        let content_selector = Selector::parse(".content").unwrap();
+        let error_message = tiles[0]
+            .select(&content_selector)
+            .next()
+            .unwrap()
+            .text()
+            .next()
+            .unwrap()
+            .trim();
+        assert_eq!(
+            "error decoding response body: EOF while parsing a value at line 1 column 0",
+            error_message
+        );
+
+        // Also check that the second tile with the helpful information is there
+        let title_selector = Selector::parse("p.title").unwrap();
+        let info_subtitle = tiles[1]
+            .select(&title_selector)
+            .next()
+            .unwrap()
+            .text()
+            .next()
+            .unwrap()
+            .trim();
+        assert_eq!("What can you do?", info_subtitle);
     }
     m.assert();
 }
