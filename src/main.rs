7da1a8e99a28690eb875f96620b77e06827d0155
Thomas Krause <thomas.krause@hu-berlin.de>
2023-08-10 14:34:54 +0200

    Allow to set a SQLite file as persistant session store

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ pub mod errors;
 pub mod state;
 mod views;
 
+use async_sqlx_session::SqliteSessionStore;
 use axum::{
     body::{self, Empty, Full},
     extract::Path,
@@ -12,10 +13,11 @@ use axum::{
     Router,
 };
 use axum_sessions::{async_session::MemoryStore, SessionLayer};
+use clap::Parser;
 use include_dir::{include_dir, Dir};
 use rand::prelude::*;
 use state::GlobalAppState;
-use std::{net::SocketAddr, sync::Arc};
+use std::{net::SocketAddr, path::PathBuf, sync::Arc, time::Duration};
 use tracing::{error, info};
 use url::Url;
 static STATIC_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/static");
@@ -41,35 +43,65 @@ async fn static_file(Path(path): Path<String>) -> Result<impl IntoResponse> {
     Ok(response)
 }
 
-fn app(addr: &SocketAddr, service_url: Option<&str>) -> Result<Router> {
+async fn app(
+    addr: &SocketAddr,
+    service_url: Option<&str>,
+    session_file: Option<&std::path::Path>,
+) -> Result<Router> {
     let mut global_state = GlobalAppState::new()?;
     global_state.frontend_prefix = Url::parse(&format!("http://{}", addr))?;
     if let Some(service_url) = service_url {
         global_state.service_url = Url::parse(service_url)?;
     }
 
-    let store = MemoryStore::new();
-    let mut secret = [0_u8; 128];
-    rand::thread_rng().fill(&mut secret);
-    let session_layer = SessionLayer::new(store, &secret).with_secure(false);
-
     let result = Router::new()
         .route("/", get(views::corpora::get))
         .route("/", post(views::corpora::post))
         .route("/static/*path", get(static_file))
-        .with_state(Arc::new(global_state))
-        .layer(session_layer);
-    Ok(result)
+        .with_state(Arc::new(global_state));
+
+    if let Some(session_file) = session_file {
+        let store =
+            SqliteSessionStore::new(&format!("sqlite://{}", session_file.to_string_lossy()))
+                .await?;
+        store.migrate().await?;
+        store.spawn_cleanup_task(Duration::from_secs(60 * 60));
+        let session_layer = SessionLayer::new(
+            store,
+            "ginoh3ya5eiLi1nohph0equ6KiwicooweeNgovoojeQuaejaixiequah6eenoo2k".as_bytes(),
+        );
+        Ok(result.layer(session_layer))
+    } else {
+        let store = MemoryStore::new();
+        let mut secret = [0_u8; 128];
+        rand::thread_rng().fill(&mut secret);
+        let session_layer = SessionLayer::new(store, &secret).with_secure(false);
+        Ok(result.layer(session_layer))
+    }
+}
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Port to listen to
+    #[arg(long, short, default_value_t = 3000)]
+    port: u16,
+    /// If set, the SQLite database file to store sessions in
+    #[arg(long)]
+    session_file: Option<PathBuf>,
 }
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
-    match app(&addr, None) {
+    let cli = Cli::parse();
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], cli.port));
+
+    match app(&addr, None, cli.session_file.as_deref()).await {
         Ok(router) => {
-            info!("Starting server with address {addr}", addr = addr);
+            info!("Starting server with address http://{addr}", addr = addr);
             let server = axum::Server::bind(&addr).serve(router.into_make_service());
             if let Err(e) = server.await {
                 error!("{}", e);
